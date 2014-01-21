fbb122fc6e56a26c249724dc810ad2eb6233a746
x711li <x711li@gmail.com>
2014-01-21 02:00:26 -0800

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function (grunt) {
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  var config = {};
+
+  grunt.initConfig(config);
+
+  var tasks = [];
+
+  grunt.registerTask('build', tasks);
+};
